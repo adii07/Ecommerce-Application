64499f69f7015568bf2dae90cbe8412c16020e9f
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-06-26 02:10:37 +0530

    "Main Activity"

--- app/src/main/java/com/aditya/bighatti/Activity/MainActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home , R.id.nav_slideshow,R.id.nav_send,R.id.nav_gallery)
+                R.id.nav_home , R.id.nav_slideshow,R.id.nav_gallery,R.id.nav_my_wishlist)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
