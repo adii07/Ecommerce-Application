0cbc9ba721212b100bf5968468d5688c7969baef
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-06-29 23:57:24 +0530

    "My Delivery Activity"

--- app/src/main/java/com/aditya/bighatti/Activity/DeliveryActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/DeliveryActivity.java
@@ -1,6 +1,8 @@
 package com.aditya.bighatti.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.aditya.bighatti.Adaptor.CartAdaptor;
 import com.aditya.bighatti.R;
@@ -15,7 +17,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class DeliveryActivity extends AppCompatActivity {
 
-    private RecyclerView cartItemsRecyclerView;
+    private RecyclerView deliveryRecyclerView;
+    private Button changeORaddNewaddressBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +31,11 @@ public class DeliveryActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Delivery");
 
 
-        cartItemsRecyclerView=findViewById(R.id.cart_items_recycler_view);
+        deliveryRecyclerView=findViewById(R.id.Delivery_recyclerView);
+        changeORaddNewaddressBTN=findViewById(R.id.change_or_add_addressBTN);
         LinearLayoutManager layoutManager=new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        cartItemsRecyclerView.setLayoutManager(layoutManager);
+        deliveryRecyclerView.setLayoutManager(layoutManager);
 
         List<CartItemModel> cartItemModelList=new ArrayList<>();
         cartItemModelList.add(new CartItemModel(0,R.drawable.amul,"Amul Chesse",1,"Rs.200","Rs. 240",2,0,0));
@@ -39,7 +43,9 @@ public class DeliveryActivity extends AppCompatActivity {
         cartItemModelList.add(new CartItemModel(0,R.drawable.amul,"Amul Chesse",1,"Rs.200","Rs. 240",2,2,0));
         cartItemModelList.add(new CartItemModel(1,"Price(3 items)","Rs. 200/-","free","Rs. 200/-","Rs. 40/-"));
         CartAdaptor cartAdaptor =new CartAdaptor(cartItemModelList);
-        cartItemsRecyclerView.setAdapter(cartAdaptor);
+        deliveryRecyclerView.setAdapter(cartAdaptor);
         cartAdaptor.notifyDataSetChanged();
+
+        changeORaddNewaddressBTN.setVisibility(View.VISIBLE);
     }
 }
