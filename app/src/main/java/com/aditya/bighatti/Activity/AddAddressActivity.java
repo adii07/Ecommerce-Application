c1d3c87a684c349946277779776e0b056089404c
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-02 23:43:58 +0530

    "Address Activity"

--- app/src/main/java/com/aditya/bighatti/Activity/AddAddressActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/AddAddressActivity.java
@@ -1,23 +1,37 @@
 package com.aditya.bighatti.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.aditya.bighatti.R;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AddAddressActivity extends AppCompatActivity {
+    private Button saveBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_address);
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar ().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle("Add Address");
+
+        saveBTN=findViewById(R.id.SaveBTN);
+        saveBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent deliveyIntent=new Intent(AddAddressActivity.this, DeliveryActivity.class);
+                startActivity(deliveyIntent);
+                finish();
+            }
+        });
     }
     //Back button
     @Override
