a4dd39b5ff660aa3312a9e6db50a6cea6fa5b680
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-05 23:58:41 +0530

    "My Addresses"

--- app/src/main/java/com/aditya/bighatti/Activity/MyAddressesActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/MyAddressesActivity.java
@@ -3,13 +3,21 @@ package com.aditya.bighatti.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.aditya.bighatti.Adaptor.AddressesAdaptor;
+import com.aditya.bighatti.Model.AddressesModel;
 import com.aditya.bighatti.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAddressesActivity extends AppCompatActivity {
 
+    private RecyclerView myaddressesRV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,20 @@ public class MyAddressesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle("MY Address");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        myaddressesRV=findViewById(R.id.addresses_RV);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        myaddressesRV.setLayoutManager(layoutManager);
+
+        List<AddressesModel> addressesModelList=new ArrayList<>();
+        addressesModelList.add(new AddressesModel("Modi","7 Race cource road","1000129"));
+        addressesModelList.add(new AddressesModel("Modi","Gujrat","100019"));
+        addressesModelList.add(new AddressesModel("Modi","Banglore","1000129"));
+        addressesModelList.add(new AddressesModel("Modi","7 Race cource road","1000129"));
+        AddressesAdaptor addressesAdaptor=new AddressesAdaptor(addressesModelList);
+        myaddressesRV.setAdapter(addressesAdaptor);
+        addressesAdaptor.notifyDataSetChanged();
+
 
     }
     @Override
