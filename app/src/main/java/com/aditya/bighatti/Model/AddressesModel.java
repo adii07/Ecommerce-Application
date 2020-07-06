dce9d95c63f093a7a295a1314b9f0193590dc525
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-06 23:54:35 +0530

    "My Address Logic"

--- app/src/main/java/com/aditya/bighatti/Model/AddressesModel.java
+++ app/src/main/java/com/aditya/bighatti/Model/AddressesModel.java
@@ -4,11 +4,21 @@ public class AddressesModel {
     private String fullName;
     private String address;
     private String pincode;
+    private Boolean selected;
 
-    public AddressesModel(String fullName, String address, String pincode) {
+    public Boolean getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Boolean selected) {
+        this.selected = selected;
+    }
+
+    public AddressesModel(String fullName, String address, String pincode,Boolean selected) {
         this.fullName = fullName;
         this.address = address;
         this.pincode = pincode;
+        this.selected=selected;
     }
 
     public String getFullName() {
