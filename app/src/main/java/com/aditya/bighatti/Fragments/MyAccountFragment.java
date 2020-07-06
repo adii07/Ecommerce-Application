c5ebba9ed5155df584ffe7a4b1567eeb729a89a2
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-06 23:54:26 +0530

    "My Address Logic"

--- app/src/main/java/com/aditya/bighatti/Fragments/MyAccountFragment.java
+++ app/src/main/java/com/aditya/bighatti/Fragments/MyAccountFragment.java
@@ -1,10 +1,13 @@
 package com.aditya.bighatti.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.aditya.bighatti.Activity.MyAddressesActivity;
 import com.aditya.bighatti.R;
 
 import androidx.fragment.app.Fragment;
@@ -15,10 +18,22 @@ public class MyAccountFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static final int MANAGE_ADDRESS=1;
+    private Button viewAllAddressBTN;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view= inflater.inflate(R.layout.fragment_my_account, container, false);
+        viewAllAddressBTN=view.findViewById(R.id.view_all_addresses_btn);
+        viewAllAddressBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), MyAddressesActivity.class);
+                intent.putExtra("MODE",MANAGE_ADDRESS);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
