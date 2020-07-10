03d3c259a7e57d164cd15152874e971539929a6a
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-11 00:20:46 +0530

    "View All Activity-Gridlayout logic"

--- app/src/main/java/com/aditya/bighatti/Activity/ViewAllActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/ViewAllActivity.java
@@ -5,6 +5,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
 
+import com.aditya.bighatti.Adaptor.GridProductLayoutAdpator;
 import com.aditya.bighatti.Adaptor.wishlistAdaptor;
 import com.aditya.bighatti.Model.WishlistModel;
 import com.aditya.bighatti.R;
@@ -32,29 +33,55 @@ public class ViewAllActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Deals of the day(DEMO)");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         recyclerView=findViewById(R.id.recyclerView);
+        gridView=findViewById(R.id.gridView);
+        int layout_code=getIntent().getIntExtra("layout_extra",-1);
 
-        recyclerView.setVisibility(View.VISIBLE);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
+        if (layout_code==0)
+        {
+            recyclerView.setVisibility(View.VISIBLE);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            layoutManager.setOrientation(RecyclerView.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
 
-        List<WishlistModel> wishlistModelList=new ArrayList<>();
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",0,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",4,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",2,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",0,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",4,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",2,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
-        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+            List<WishlistModel> wishlistModelList = new ArrayList<>();
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 1, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 0, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 4, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 2, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 1, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 1, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 0, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 4, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 2, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
+            wishlistModelList.add(new WishlistModel(R.drawable.image7, "Lotte ChocoPie", 1, "4.7", 1169, "Rs. 100/-", "Rs. 120", "Cash On Delivery"));
 
-        wishlistAdaptor adaptor=new wishlistAdaptor(wishlistModelList,false);
-        recyclerView.setAdapter(adaptor);
-        adaptor.notifyDataSetChanged();
+            wishlistAdaptor adaptor = new wishlistAdaptor(wishlistModelList, false);
+            recyclerView.setAdapter(adaptor);
+            adaptor.notifyDataSetChanged();
+        }
 
 
+
+        else if (layout_code ==1)
+        {
+            gridView.setVisibility(View.VISIBLE);
+            List<HorizontalProductModel> horizontalProductModelList = new ArrayList<>();
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image, "Aashirvaad Aata", "10kg", "Rs. 590"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.amul, "Amul Chesse", "120g", "Rs. 100"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image2, "Royal Aata", "10kg", "Rs. 540"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image3, "Hershey Syrup", "450 g", "Rs. 200"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image4, "Nestle Ice Tea", "1L", "Rs. 120"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image5, "Roofafza", "750mL", "Rs. 150"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image6, "Hide And Seek", "120g", "Rs. 30"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image7, "Lotte Choco Pie", "300g", "Rs. 140"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image8, "Little Hearts", "18g", "Rs. 10"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image5, "Roofafza", "750mL", "Rs. 150"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image3, "Hershey Syrup", "450 g", "Rs. 200"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image4, "Nestle Ice Tea", "1L", "Rs. 120"));
+            horizontalProductModelList.add(new HorizontalProductModel(R.drawable.image5, "Roofafza", "750mL", "Rs. 150"));
+            GridProductLayoutAdpator adpator = new GridProductLayoutAdpator(horizontalProductModelList);
+            gridView.setAdapter(adpator);
+        }
     }
 
     //Back Button
