46feac14cea91221fec65a9add73c7b51296bd45
Aditya Sharma <43462608+adii07@users.noreply.github.com>
2020-07-10 23:51:09 +0530

    "View All Activity-RecyclerView logic"

--- app/src/main/java/com/aditya/bighatti/Activity/ViewAllActivity.java
+++ app/src/main/java/com/aditya/bighatti/Activity/ViewAllActivity.java
@@ -2,14 +2,25 @@ package com.aditya.bighatti.Activity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
 
+import com.aditya.bighatti.Adaptor.wishlistAdaptor;
+import com.aditya.bighatti.Model.WishlistModel;
 import com.aditya.bighatti.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ViewAllActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,28 @@ public class ViewAllActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle("Deals of the day(DEMO)");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        recyclerView=findViewById(R.id.recyclerView);
 
+        recyclerView.setVisibility(View.VISIBLE);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<WishlistModel> wishlistModelList=new ArrayList<>();
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",0,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",4,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",2,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",0,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",4,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",2,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+        wishlistModelList.add(new WishlistModel(R.drawable.image7,"Lotte ChocoPie",1,"4.7",1169,"Rs. 100/-","Rs. 120","Cash On Delivery"));
+
+        wishlistAdaptor adaptor=new wishlistAdaptor(wishlistModelList,false);
+        recyclerView.setAdapter(adaptor);
+        adaptor.notifyDataSetChanged();
 
 
     }
